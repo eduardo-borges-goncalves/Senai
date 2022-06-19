2909e3524eb978f32c5578005c7d64c23f5b8436
Eduardo Borges <alemaobrk1@gmail.com>
2022-06-19 15:05:08 -0300

    first commit

---
+++ C#/Semana-02/aula-01/FistC#.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string priest = "Fábio de Melo";
+string comedian = "Tatá werneck";
+
+Console.WriteLine($"Hello {priest} e {comedian}!");
