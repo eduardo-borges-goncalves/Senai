5079fb6a2b9849a3183909e0c2a39225a91eacda
Eduardo Borges <alemaobrk1@gmail.com>
2022-06-19 15:08:10 -0300

    first commit

--- C#/Semana-02/aula-02/exercize/Program.cs
+++ C#/Semana-02/aula-02/exercize/Program.cs
@@ -1,46 +1,46 @@
 ﻿// Exercício 1
 
-// Console.WriteLine($"Favor insira um número");
-// int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Favor insira um número");
+int number1 = int.Parse(Console.ReadLine());
 
-// Console.WriteLine($"Favor insira outro número");
-// int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Favor insira outro número");
+int number2 = int.Parse(Console.ReadLine());
 
-// Console.WriteLine($"O resultado da soma é {number1 + number2}");
+Console.WriteLine($"O resultado da soma é {number1 + number2}");
 
 // Exercício 2 
 
-// Console.WriteLine($"Favor insira o nome do paciente");
-// string patient = Console.ReadLine();
+Console.WriteLine($"Favor insira o nome do paciente");
+string patient = Console.ReadLine();
 
-// Console.WriteLine($"Favor insira a idade do paciente");
-// int age = int.Parse(Console.ReadLine());
+Console.WriteLine($"Favor insira a idade do paciente");
+int age = int.Parse(Console.ReadLine());
 
-// string faixaEtaria;
+string faixaEtaria;
 
-// if (age < 18) {
-//   faixaEtaria = "menor de idade";
-// } else if (age < 60) { 
-//   faixaEtaria = "Maior de Idade, Adulto";
-// } else {
-//   faixaEtaria = "idoso.";
-// }
+if (age < 18) {
+  faixaEtaria = "menor de idade";
+} else if (age < 60) { 
+  faixaEtaria = "Maior de Idade, Adulto";
+} else {
+  faixaEtaria = "idoso.";
+}
 
-// Console.WriteLine($"O paciente {patient} tem {age} anos e é {faixaEtaria}.");
+Console.WriteLine($"O paciente {patient} tem {age} anos e é {faixaEtaria}.");
 
 // Exercício 3 
 
-// Console.WriteLine($"Favor insira o nome do veículo");
-// string model = Console.ReadLine();
+Console.WriteLine($"Favor insira o nome do veículo");
+string model = Console.ReadLine();
 
-// Console.WriteLine($"Favor insira a marca do veículo");
-// string brand = Console.ReadLine();
+Console.WriteLine($"Favor insira a marca do veículo");
+string brand = Console.ReadLine();
 
-// Console.WriteLine($"Favor insira a quilometragem do veículo");
-// int quilometragem = int.Parse(Console.ReadLine());
+Console.WriteLine($"Favor insira a quilometragem do veículo");
+int quilometragem = int.Parse(Console.ReadLine());
 
-// if (quilometragem > 10000) {
-//   Console.WriteLine($"O veículo {model} necessita de revisão. ");
-// } else {
-//   Console.WriteLine($"O veículo {model} está em bom estado e não necessita de revisão no momento.");
-// }
+if (quilometragem > 10000) {
+  Console.WriteLine($"O veículo {model} necessita de revisão. ");
+} else {
+  Console.WriteLine($"O veículo {model} está em bom estado e não necessita de revisão no momento.");
+}
