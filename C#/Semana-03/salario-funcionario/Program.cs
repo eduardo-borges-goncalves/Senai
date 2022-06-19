b233b94f196ec2a76c5fd9878d543badac8e43c8
Eduardo Borges <alemaobrk1@gmail.com>
2022-06-19 15:31:33 -0300

    implementa ex-01 s-03

---
+++ C#/Semana-03/salario-funcionario/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program {
+  static void Main (string [] args ) {
+    Console.Write("Digite o nome do funcionário: ");
+    string name = Console.ReadLine();
+    Console.Write("Digite o ano de nascimento: ");
+    int year = int.Parse(Console.ReadLine());
+    Console.Write("Digite o salário do funcionário: ");
+    double salary = double.Parse(Console.ReadLine());
+    Console.Write("Digite a porcentagem do reajuste: ");
+    double readjustment = double.Parse(Console.ReadLine());
+
+    Console.WriteLine(
+      $"{name} têm {2022 - year} anos e ganha R$ {salary + ((salary * readjustment) / 100)}");
+
+
+
+
+
+  }
+}
